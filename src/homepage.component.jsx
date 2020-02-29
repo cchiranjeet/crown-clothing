0fa2a66059841e19b7bfa622810e19594639d79f
chiranjeet chauhan <cchiranjeet@gmail.com>
2020-02-29 13:27:35 +0530

    setup ecomerce project

---
+++ src/homepage.component.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./homepage.style.scss";
+const HomePage = () => (
+  <div className="homwpage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">jackets</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">shoes</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">mens</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Whomens</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default HomePage;
