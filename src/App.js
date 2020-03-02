9b5d57f323e65597dd746a89be8a81cf66c32fc5
chughjitin <jitin.chugh@espire.com>
2020-03-02 15:25:13 +0530

    redirect option

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route,Redirect } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
@@ -8,6 +8,7 @@ import SigninAndSignupPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import {setCurrentUser} from './redux/user/user.action'
+import userReducer from "./redux/user/user.reducer";
 
 class App extends React.Component {
   unsubscribeAuthUser = null;
@@ -41,13 +42,16 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/sign-in" component={SigninAndSignupPage} />
+          <Route path="/sign-in" render={()=> this.props.currentUser?(<Redirect to='/'/>):(<SigninAndSignupPage/>)}  />
         </Switch>
       </div>
     );
   }
 }
+const matoStateToProps =({user})=>({
+  currentUser:user.currentUser
+})
 const mapDispatchToProps = dispatch =>({
   setCurrentUser:user => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps) (App);
+export default connect(matoStateToProps, mapDispatchToProps) (App);
